fbe24d4f9375d27b70c46e9bd505f242d01d1060
Francis <francisglacken@gmail.com>
2018-10-11 02:52:47 -0400

    Initial commit

---
+++ app/src/main/java/com/deltorostudios/recviewcodepathtutorial/UserListActivity.java
@@ -0,0 +1,39 @@
+package com.deltorostudios.recviewcodepathtutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class UserListActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Lookup the recyclerview in activity Layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+
+        // Set Layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+
+        // That's all folks!
+
+
+    }
+
+}
